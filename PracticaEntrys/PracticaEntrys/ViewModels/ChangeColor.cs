2bed9b60b457876bae82d67a6b1bf9befe71e743
SkimoCHK <alexguillen265@gmail.com>
2024-01-16 09:13:33 -0700

    Agregar archivos de proyecto.

---
+++ PracticaEntrys/PracticaEntrys/ViewModels/ChangeColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PracticaEntrys.ViewModels
+{
+    public class ChangeColor : TriggerAction<Button>
+    {
+        protected override void Invoke(Button btn)
+        {
+            btn.BackgroundColor = Color.DarkOrange;
+            btn.Text = "Here we go!!!";
+        }
+    }
+}
---
+++ PracticaEntrys/PracticaEntrys/ViewModels/VMpage1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PracticaEntrys.ViewModels
+{
+    public class VMpage1 : BaseViewModel
+    {
+        public VMpage1(INavigation navigation ) 
+        {
+         Navigation = navigation;
+        }
+
+        public class ChangeColor : TriggerAction<Button>
+        {
+            protected override void Invoke(Button btn)
+            {
+                btn.BackgroundColor = Color.DarkOrange;
+                btn.Text = "Here we go!!!";
+            }
+        }
+
+    }
+
+}
